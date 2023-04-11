3673a5682aa888a53df6447da2d1841f8715cee9
JQ-DataScience <q-jan@live.fr>
2023-04-11 18:38:44 +0200

    create data_dl & update test_back_pytest

--- test_back_pytest.py
+++ test_back_pytest.py
@@ -4,14 +4,6 @@ import back
 from flask import json
 
 
-# Test sur l'existence du fichier data_OHE.csv
-if not os.path.isfile('data_OHE.csv'):
-    url = "https://gist.githubusercontent.com/JQ-DataScience/6ee7f21195acceee0b70e1162a03d087/raw/bd8ad2a095021511256972df87ea1839f4ca8bc5/data_OHE.csv"
-    response = requests.get(url)
-    with open('data_OHE.csv', 'wb') as f:
-        f.write(response.content)
-    assert os.path.isfile('data_OHE.csv')
-
 
 # Test sur l'existance et l'extension du fichier back.py
 def test_back_file_extension():
