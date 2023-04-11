5bc5e2136b13f82b4154fb71ac5c2c7d4ec162bc
JQ-DataScience <q-jan@live.fr>
2023-04-11 19:19:36 +0200

    update data_dl

--- data_dl.py
+++ data_dl.py
@@ -1,5 +1,6 @@
 import os
 import requests
+import boto3
 
 # Test sur l'existence du fichier data_OHE.csv
 if not os.path.isfile('data_OHE.csv'):
@@ -7,3 +8,12 @@ if not os.path.isfile('data_OHE.csv'):
     response = requests.get(url)
     with open('data_OHE.csv', 'wb') as f:
         f.write(response.content)
+
+
+
+# vérifier l'existence du fichier "best_model.pkl"
+if not os.path.isfile("best_model.pkl"):
+    # créer une session boto3 S3 et télécharger le fichier depuis le lien S3
+    s3 = boto3.resource('s3')
+    s3.Bucket('monlien').download_file('model.pkl', 'best_model.pkl')
+
