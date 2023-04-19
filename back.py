067b45221f7619331994ab289f35ee117d67e7bb
JQ-DataScience <q-jan@live.fr>
2023-04-19 22:41:48 +0200

    back update

--- back.py
+++ back.py
@@ -95,7 +95,7 @@ def trigger_deployment():
    
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',debug=True, port=5000)
+    app.run(debug=True, port=5000)
 """
 # Fonction pour lancer l'application Flask 2
 def run_app2():
